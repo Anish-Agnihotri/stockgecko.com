baa65d1a8ece9f27f588565f10873981ebf3f98a
anish-agnihotri <contact@anishagnihotri.com>
2026-02-11 20:35:49 -0500

    fix: Svelte Vercel adapter, postinstall

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
-import adapter from "@sveltejs/adapter-auto";
+import adapter from "@sveltejs/adapter-vercel";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	kit: {
-		adapter: adapter(),
+		adapter: adapter({ runtime: "experimental_bun1.x" }),
 		alias: {
 			$config: "src/config",
 			$workflows: "src/workflows",
