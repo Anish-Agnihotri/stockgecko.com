ebf1033aac761e97a358c20a0c3ede962a114b88
anish-agnihotri <contact@anishagnihotri.com>
2026-02-13 15:29:31 -0500

    fix: cleanup AssetTable setup

--- src/lib/components/AssetTable.svelte
+++ src/lib/components/AssetTable.svelte
@@ -10,101 +10,81 @@
 	// Collect data snapshot
 	const getSnapshot = getContext<() => DiffedSnapshot>("snapshot");
 	const snapshot = $derived(getSnapshot());
+
+	// Weaker types for ease (indexing configs)
+	type Meta = { name: string; icon: string[] };
+	type ExchangeCfg = Record<string, Meta>;
+	type TickerCfg = Record<string, Record<string, { meta: Meta }>>;
 </script>
 
-<!-- Scrollable handler on mobile -->
-<div
-	class="border-b border-b-gecko-shade bg-gecko-black px-2 py-0.5 text-right font-mono text-xs text-gecko-gray/30 uppercase md:hidden"
->
-	<!-- FIXME: it annoys me a little that the arrow is not vertically aligned -->
-	<span>Scrollable <span class="text-lg">↔</span> </span>
-</div>
-
 <Table.Root class="w-full min-w-3xl table-fixed">
+	<!-- Ranking chg., ranking, asset, midPx, midPx change, vol, vol change, venues -->
 	<Table.Header class="bg-gecko-black">
-		<Table.Row class="border-b-gecko-shade text-xs font-light">
-			<!-- Ranking change -->
-			<Table.Head class="w-8 px-0"></Table.Head>
-			<!-- Ranking -->
-			<Table.Head class="w-8 px-0"></Table.Head>
-			<!-- Asset details -->
-			<Table.Head class="px-0">Asset</Table.Head>
-			<!-- Mid px -->
-			<Table.Head class="w-24 px-0">Mid Price</Table.Head>
-			<!-- Mid px change -->
-			<Table.Head class="w-24 px-0">24h</Table.Head>
-			<!-- Volume -->
-			<Table.Head class="w-24 px-0">Volume</Table.Head>
-			<!-- Volume change -->
-			<Table.Head class="w-24 px-0">24h</Table.Head>
-			<!-- Venues -->
-			<Table.Head class="w-30 px-0">Venues</Table.Head>
+		<Table.Row class="border-b-gecko-shade text-xs font-light [&_th]:px-0">
+			<Table.Head class="w-10"></Table.Head>
+			<Table.Head class="w-6"></Table.Head>
+			<Table.Head>Asset</Table.Head>
+			<Table.Head class="w-24">Mid Price</Table.Head>
+			<Table.Head class="w-24">24h</Table.Head>
+			<Table.Head class="w-24">Volume</Table.Head>
+			<Table.Head class="w-24">24h</Table.Head>
+			<Table.Head class="w-30">Venues</Table.Head>
 		</Table.Row>
 	</Table.Header>
 
 	<Table.Body>
-		{#each snapshot.index.assetsByVolume as { asset, previousIndex }, i}
-			{@const assetData = snapshot.assets[asset]}
-			{@const assetMetadata = tickers.perps[assetData.category][asset].meta}
+		{#each snapshot.index.assetsByVolume as { asset: assetId, previousIndex }, i}
+			<!-- Collect asset data + metadata -->
+			{@const asset = snapshot.assets[assetId]}
+			{@const { name, icon } = (tickers.perps as TickerCfg)[asset.category][assetId].meta}
+			{@const rankDelta = previousIndex != null ? previousIndex - i : 0}
 
 			<Table.Row
-				class="h-10 border-b-gecko-shade text-xs transition-none hover:bg-gecko-black-hover"
+				class="h-10 border-b-gecko-shade text-xs transition-none hover:bg-gecko-black-hover [&_td]:px-0 [&_td]:text-left [&_td]:align-middle"
 			>
 				<!-- Ranking change -->
-				<Table.Cell class="w-8 px-0 text-center align-middle">
-					<Numeric value={previousIndex ? previousIndex - i : 0} change />
-				</Table.Cell>
+				<Table.Cell class="w-10 text-center!"><Numeric value={rankDelta} change /></Table.Cell>
 
 				<!-- Current ranking -->
-				<Table.Cell class="w-8 px-0 text-center align-middle"><Numeric value={i + 1} /></Table.Cell>
+				<Table.Cell class="w-6"><Numeric value={i + 1} /></Table.Cell>
 
 				<!-- Asset -->
-				<Table.Cell class="pr-0 pl-2 text-left align-middle">
+				<Table.Cell class="pr-0 pl-2">
 					<span class="flex items-center">
-						<Icon src={assetMetadata.icon} alt={assetMetadata.name} />
-						<span class="ml-2 text-gecko-white">{assetMetadata.name}</span>
-						<span class="ml-1 text-gecko-gray">{asset.toUpperCase()}</span>
+						<Icon src={icon} alt={name} />
+						<span class="ml-2 text-gecko-white">{name}</span>
+						<span class="ml-1 text-gecko-gray">{assetId.toUpperCase()}</span>
 					</span>
 				</Table.Cell>
 
 				<!-- Mid price -->
-				<Table.Cell class="w-24 px-0 text-left align-middle"
-					><Numeric
-						value={assetData.medianMidPx}
-						format="numeric"
-						dollar
-						class="text-gecko-white"
-					/></Table.Cell
-				>
+				<Table.Cell class="w-24">
+					<Numeric value={asset.medianMidPx} format="numeric" dollar class="text-gecko-white" />
+				</Table.Cell>
 
 				<!-- Mid price change -->
-				<Table.Cell class="w-20 px-0 text-left align-middle">
-					<Numeric value={assetData.medianMidPxChange * 100} format="numeric" change percentage />
+				<Table.Cell class="w-20">
+					<Numeric value={asset.medianMidPxChange * 100} format="numeric" change percentage />
 				</Table.Cell>
 
 				<!-- Volume -->
-				<Table.Cell class="w-24 px-0 text-left align-middle">
-					<Numeric value={assetData.volume} format="currency" dollar class="text-gecko-white" />
+				<Table.Cell class="w-24">
+					<Numeric value={asset.volume} format="currency" dollar class="text-gecko-white" />
 				</Table.Cell>
 
 				<!-- Volume change -->
-				<Table.Cell class="w-20 px-0 text-left align-middle">
-					<Numeric value={assetData.volumeChange * 100} format="numeric" change percentage />
+				<Table.Cell class="w-20">
+					<Numeric value={asset.volumeChange * 100} format="numeric" change percentage />
 				</Table.Cell>
 
 				<!-- Venues -->
-				<Table.Cell class="w-30 px-0 text-left align-middle">
-					<span class="flex flex-row items-center space-x-0.5">
-						{#each assetData.marketIds as marketId}
-							{@const market = snapshot.markets[marketId]}
-							{@const venueMetadata = exchanges[`${market.venue}:${market.namespace}`]}
+				<Table.Cell class="w-30">
+					<span class="flex flex-row items-center gap-0.5">
+						{#each asset.marketIds as marketId}
+							{@const { venue, namespace } = snapshot.markets[marketId]}
+							{@const { name, icon } = (exchanges as ExchangeCfg)[`${venue}:${namespace}`]}
 
-							<Icon
-								src={venueMetadata.icon}
-								alt={venueMetadata.name}
-								nested={true}
-								tooltip={venueMetadata.name}
-							/>
+							<Icon src={icon} alt={name} tooltip={name} nested />
 						{/each}
 					</span>
 				</Table.Cell>
