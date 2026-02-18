a90ee299192b8ee4e37e547b929222fc19c2ec3a
anish-agnihotri <contact@anishagnihotri.com>
2026-02-18 15:50:32 -0500

    refactor: drop HL lib dep, fix OI calculation

--- src/workflows/collection/hyperliquid.ts
+++ src/workflows/collection/hyperliquid.ts
@@ -1,105 +1,103 @@
-import { fetch } from "workflow";
+import { Decimal } from "decimal.js";
+import { stepFetchJSON } from "$workflows/shared/fetch";
 import type { MarketEntryCreateInput } from "$prisma/models";
 import { stepInsertMarketEntries } from "$workflows/shared/db";
-import { HttpTransport, InfoClient } from "@nktkas/hyperliquid";
 import { stepValidateMarkets } from "$workflows/shared/validate";
-import type { AsyncReturn, MethodKeys, MethodAt } from "$lib/types";
 
-/**
- * Generic Hyperliquid API `/info` call via typed `@nktas/hyperliquid.InfoClient` method
- * @param {M} Method to call (e.g, `perpDexs`)
- * @param {Parameters<MethodAt<InfoClient, M>>} args strongly-typed parameters of the method call
- * @returns {Promise<AsyncReturn<MethodAt<InfoClient, M>>>} inferred async method response
- */
-async function stepCollectHLInfo<M extends MethodKeys<InfoClient>>(
-	method: M,
-	...args: Parameters<MethodAt<InfoClient, M>>
-): Promise<AsyncReturn<MethodAt<InfoClient, M>>> {
-	"use step";
+// Hyperliquid Info API url
+const H_API_URL = "https://api.hyperliquid.xyz/info";
 
-	// Setup client library
-	const transport = new HttpTransport();
-	const info = new InfoClient({ transport });
+// `"type": "allPerpMetas"` response subset
+type AllPerpMetas = {
+	universe: {
+		name: string;
+	}[];
+}[];
 
-	// Collect and run function
-	// @dev: not particularly happy camper by this type definition, but we
-	// 			 are effectively just constraining the function type
-	const fn = info[method].bind(info) as (
-		...a: Parameters<MethodAt<InfoClient, M>>
-	) => ReturnType<MethodAt<InfoClient, M>>;
-	return await fn(...args);
-}
+// `"type": "metaAndAssetCtxs"` response subset
+type MetaAndAssetCtxsResponse = [
+	{
+		universe: {
+			name: string;
+			maxLeverage: number;
+		}[];
+	},
+	{
+		openInterest: string;
+		dayNtlVlm: string;
+		oraclePx: string;
+		markPx: string;
+		midPx: string | null;
+	}[]
+];
 
 /**
  * Collect relevant Hyperliquid market data
- * @returns workflow execution diagnostics
+ * @returns execution diagnostics
  */
 export async function collectHyperliquidMarkets(batchId: string): Promise<{
-	collectedDexs: readonly string[];
 	insertedMarkets: number;
-	failures: { dex: string; error: string }[];
 }> {
 	"use workflow";
 
-	// `@nktas/hyperliquid.HttpTransport` uses `fetch`
-	// Patch `fetch` globally to make workflow-safe
-	globalThis.fetch = fetch;
+	// Fetch perp meta info
+	const meta = await stepFetchJSON<AllPerpMetas>(H_API_URL, false, {
+		method: "POST",
+		headers: { "Content-Type": "application/json" },
+		body: JSON.stringify({ type: "allPerpMetas" })
+	});
 
-	// Fetch Hyperliquid perp dex info
-	const dexInfo = await stepCollectHLInfo("perpDexs");
-
-	// Using fetched data, build a set of all valid Hyperliquid markets
-	const validMarkets: ReadonlySet<string> = dexInfo.reduce((acc, cat) => {
-		cat?.assetToStreamingOiCap?.forEach((asset) => acc.add(asset[0]));
-		return acc;
-	}, new Set<string>());
+	// Setup set of valid markets
+	const validMarkets = meta.flatMap(({ universe }) => universe.map(({ name }) => name));
 
 	// Validate config
-	await stepValidateMarkets("hyperliquid", validMarkets);
+	await stepValidateMarkets("hyperliquid", new Set(validMarkets));
 
 	// We filter over all valid DEXes and collect data optimisticaly,
 	// even if said DEXes do not contain relevant tracked data.
 	//
 	// This way, in the future where they add new markets, we can drop in
 	// add with historic data.
-	const relevantDexNames: readonly string[] = [
-		...new Set([...validMarkets].map((market) => market.split(":")[0]))
+	const dexs: readonly string[] = [
+		...new Set(
+			[...validMarkets]
+				// Ignore non-HIP-3 dex data
+				.filter((market) => market.includes(":"))
+				// Grab just HIP-3 dex name
+				.map((market) => market.split(":")[0])
+		)
 	];
 
 	// Fanout: fetch data for each dex in parallel
-	const settled = await Promise.allSettled(
-		relevantDexNames.map((dex) => stepCollectHLInfo("metaAndAssetCtxs", { dex }))
+	const response = await Promise.all(
+		dexs.map((dex) =>
+			stepFetchJSON<MetaAndAssetCtxsResponse>(H_API_URL, false, {
+				method: "POST",
+				headers: { "Content-Type": "application/json" },
+				body: JSON.stringify({ type: "metaAndAssetCtxs", dex })
+			})
+		)
 	);
 
 	// Parse collected responses
 	const rows: MarketEntryCreateInput[] = [];
-	const failures: { dex: string; error: string }[] = [];
 
-	for (let i = 0; i < settled.length; i++) {
-		// Collect `dex` name and associated settled response
-		const dex = relevantDexNames[i];
-		const res = settled[i];
-
-		// If response failed, track failure
-		if (res.status === "rejected") {
-			failures.push({ dex, error: String(res.reason) });
-			continue;
+	// Iterate over response
+	response.forEach(([{ universe }, metrics]) => {
+		// If no values, continue
+		if (universe.length == 0) {
+			return;
 		}
 
-		// Destructure relevant data
-		const [{ universe }, metrics] = res.value;
+		// Grab dex name
+		const dex = universe[0].name.split(":")[0];
 
-		// Associate market metrics to relevant market details
-		// First, ensure sufficient metrics for each market
-		//
 		// In a well structured response, the `universe` of all available markets
 		// corresponds 1:1 with the set of all available market `metrics`
 		if (universe.length !== metrics.length) {
-			failures.push({ dex, error: "Markets length does not match metrics length" });
-			continue;
+			throw new Error(`Markets length does not match metrics length (dex: ${dex})`);
 		}
 
-		// Append market data for this `dex` to inserted `rows`
 		rows.push(
 			...universe.map((market, i) => ({
 				batchId,
@@ -111,24 +109,20 @@ export async function collectHyperliquidMarkets(batchId: string): Promise<{
 				maxLeverage: market.maxLeverage,
 
 				// Market metrics
-				midPx: metrics[i].midPx ?? "0",
+				midPx: metrics[i].midPx ?? 0,
 				markPx: metrics[i].markPx,
 				oraclePx: metrics[i].oraclePx,
-				prevDayPx: metrics[i].prevDayPx,
-				oi: metrics[i].openInterest,
-				funding: metrics[i].funding,
-				volume: metrics[i].dayNtlVlm
+				volume: metrics[i].dayNtlVlm,
+
+				// OI (default denominated in units not dollar notional)
+				oi: new Decimal(metrics[i].openInterest).mul(metrics[i].midPx ?? 0).toNumber()
 			}))
 		);
-	}
+	});
 
 	// Insert collected data to database
-	const insertedCount = await stepInsertMarketEntries(rows);
+	const insertedMarkets = await stepInsertMarketEntries(rows);
 
-	// Return workflow execution diagnostics
-	return {
-		collectedDexs: relevantDexNames,
-		insertedMarkets: insertedCount,
-		failures
-	};
+	// Return execution diagnostics
+	return { insertedMarkets };
 }
