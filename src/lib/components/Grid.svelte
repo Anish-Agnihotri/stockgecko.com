fa611a95637599043febf8b9ab26f28b5964712f
anish-agnihotri <contact@anishagnihotri.com>
2026-02-19 13:04:18 -0500

    feat: impl. initial Markets page

---
+++ src/lib/components/Grid.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import type { Snippet } from "svelte";
+
+	let { children, bottom = true }: { children: Snippet; bottom?: boolean } = $props();
+</script>
+
+<div class="flex flex-1 flex-col">
+	<!-- Top section border -->
+	<div class="hidden h-5 w-full justify-between border-b border-b-gecko-shade md:flex md:h-8">
+		<div class="h-5 w-5.25 border-r border-r-gecko-shade md:h-8 md:w-8.25"></div>
+		<div class="h-5 w-5.25 border-l border-l-gecko-shade md:h-8 md:w-8.25"></div>
+	</div>
+
+	<!-- Content section -->
+	<div
+		class="flex flex-1 flex-col px-0 md:flex-row md:gap-8 md:px-8 [&>div]:border-b [&>div]:border-b-gecko-shade [&>div]:bg-gecko-shade/25 md:[&>div]:border-x md:[&>div]:border-b-0 md:[&>div]:border-x-gecko-shade"
+	>
+		{@render children()}
+	</div>
+
+	<!-- Bottom section border -->
+	{#if bottom}
+		<!-- If visible, standard to top-->
+		<div class="hidden h-5 w-full justify-between border-t border-t-gecko-shade md:flex md:h-8">
+			<div class="h-5 w-5.25 border-r border-r-gecko-shade md:h-8 md:w-8.25"></div>
+			<div class="h-5 w-5.25 border-l border-l-gecko-shade md:h-8 md:w-8.25"></div>
+		</div>
+	{:else}
+		<!-- If not visible, on large displays, make sure to handle "bottom" border -->
+		<div class="hidden border-t border-t-gecko-shade md:flex"></div>
+	{/if}
+</div>
--- src/routes/markets/+page.svelte
+++ src/routes/markets/+page.svelte
@@ -1,1 +1,90 @@
-<span>TODO: page of all markets</span>
+<script lang="ts">
+	import { getContext } from "svelte";
+	import Icon from "$components/Icon.svelte";
+	import Grid from "$components/Grid.svelte";
+	import exchanges from "$config/exchanges.json";
+	import Numeric from "$components/Numeric.svelte";
+	import type { DiffedSnapshot } from "$lib/transform";
+	import MarketTable from "$components/table/MarketTable.svelte";
+
+	// Collect data snapshot
+	const getSnapshot = getContext<() => DiffedSnapshot>("snapshot");
+	const snapshot = $derived(getSnapshot());
+
+	// Stats
+	const venueCount = Object.keys(exchanges).length;
+	const marketCount = Object.keys(snapshot.markets).length;
+</script>
+
+<!-- Landing header (title, description) -->
+<Grid bottom={false}>
+	<div class="flex flex-1 flex-col gap-0.5 px-4 py-6">
+		<h1 class="text-lg text-gecko-white md:text-xl">Markets</h1>
+		<p class="text-sm text-gecko-gray/75">
+			Aggregating {marketCount} markets across {venueCount} venues.
+		</p>
+	</div>
+</Grid>
+
+<!-- Statistics row -->
+<Grid>
+	<div class="flex flex-1 flex-col md:flex-6 lg:flex-9">
+		<!-- Chart header -->
+		<div
+			class="flex h-10 items-center border-b border-b-gecko-shade bg-gecko-black px-4 text-xs font-medium"
+		>
+			<h3>Volume vs. Open Interest</h3>
+		</div>
+
+		<!-- Chart body -->
+		<div class="flex min-h-30 flex-1 flex-col items-center justify-center text-xs text-gecko-muted">
+			<span>TODO</span>
+		</div>
+	</div>
+
+	<!-- Exchange dominance by OI -->
+	<div class="flex flex-1 flex-col md:flex-3">
+		<!-- Table header -->
+		<div class="flex h-10 items-center bg-gecko-black px-4 text-xs font-medium">
+			<h3>Open Interest Dominance</h3>
+		</div>
+
+		<!-- Table content -->
+		<div class="flex flex-col">
+			{#each snapshot.aggregates.oiByVenue as { venue, oiShare }, i}
+				{@const { name, icon } = exchanges[`${venue}:` as keyof typeof exchanges]}
+
+				<a
+					href="/venue/{venue}"
+					class="flex h-10 items-center justify-between border-t border-t-gecko-shade text-xs hover:bg-gecko-black-hover"
+				>
+					<div class="flex items-center">
+						<!-- Rank -->
+						<span class="w-10 text-center"><Numeric value={i + 1} /></span>
+
+						<!-- Exchange -->
+						<div class="flex items-center">
+							<div class="flex w-7 items-center justify-center">
+								<Icon src={icon} alt={name} />
+							</div>
+							<span class="ml-2 text-gecko-white">{name}</span>
+						</div>
+					</div>
+
+					<!-- OI share -->
+					<span class="pr-4"
+						><Numeric
+							value={oiShare * 100}
+							format="numeric"
+							percentage
+							class="text-gecko-gray!"
+						/></span
+					>
+				</a>
+			{/each}
+		</div>
+	</div>
+</Grid>
+
+<!-- Table of all markets -->
+<MarketTable />
