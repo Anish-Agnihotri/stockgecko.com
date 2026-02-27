bf08aba2f0faa6911ba8f5cbe83f0825cf14118c
anish-agnihotri <contact@anishagnihotri.com>
2026-02-27 13:24:32 -0500

    perf: naive: viewport lazy-load all images

--- src/lib/components/Icon.svelte
+++ src/lib/components/Icon.svelte
@@ -17,7 +17,7 @@ Size should be sane default
 <!-- Single icon -->
 {#if src.length == 1}
 	<div class={extraClass}>
-		<img src={src[0]} {alt} class="size-5 rounded-full" />
+		<img src={src[0]} {alt} class="size-5 rounded-full" loading="lazy" />
 	</div>
 {/if}
 
@@ -28,11 +28,13 @@ Size should be sane default
 			src={src[0]}
 			alt={`${alt} pair 1`}
 			class="absolute bottom-0.5 left-0.5 z-10 size-3.75 rounded-full"
+			loading="lazy"
 		/>
 		<img
 			src={src[1]}
 			alt={`${alt} pair 2`}
 			class="absolute top-0.5 right-0.5 z-0 size-3.75 rounded-full"
+			loading="lazy"
 		/>
 	</div>
 {/if}
@@ -40,7 +42,17 @@ Size should be sane default
 <!-- Exchange icon (nested) -->
 {#if src.length == 2 && nested}
 	<div class="{extraClass} relative size-5">
-		<img src={src[1]} {alt} class="absolute top-0 left-px z-0 size-4.25 rounded-full" />
-		<img src={src[0]} {alt} class="absolute right-0 bottom-0 z-10 size-2.5 rounded-full" />
+		<img
+			src={src[1]}
+			{alt}
+			class="absolute top-0 left-px z-0 size-4.25 rounded-full"
+			loading="lazy"
+		/>
+		<img
+			src={src[0]}
+			{alt}
+			class="absolute right-0 bottom-0 z-10 size-2.5 rounded-full"
+			loading="lazy"
+		/>
 	</div>
 {/if}
